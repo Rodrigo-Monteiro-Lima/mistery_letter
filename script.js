ddeb8b12183361a628cec933f6bce89462ee2912
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-06 09:45:50 -0300

    Resolução do sexto requisito

--- script.js
+++ script.js
@@ -7,25 +7,28 @@ function clearSpan() {
   }
 }
 
-function emptyInput() {
-  if (input === '' || input === ' ') {
+// Expressão regular foi pega na página: https://pt.stackoverflow.com/questions/58498/se-input-tiver-apenas-espa%C3%A7os-em-branco-n%C3%A3o-fazer-nada
+function emptyInput(input) {
+  if (input === '' || input.match(/^(\s)+$/)) {
     paragraph.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    const array = input.split(' ');
+    paragraph.innerHTML = '';
+    for (let index = 0; index < array.length; index += 1) {
+      const word = document.createElement('span');
+      word.innerHTML = array[index];
+      paragraph.appendChild(word);
+    }
   }
 }
 
 const btnGenerate = document.querySelector('#criar-carta');
-const input = document.querySelector('#carta-texto').value;
 const paragraph = document.querySelector('#carta-gerada');
 btnGenerate.addEventListener('click', () => {
-  const array = input.split(' ');
-  console.log(array);
+  const input = document.querySelector('#carta-texto').value;
+  console.log(input);
   clearSpan();
-  emptyInput();
-  for (let index = 0; index < array.length; index += 1) {
-    const word = document.createElement('span');
-    word.innerHTML = array[index];
-    paragraph.appendChild(word);
-  }
+  emptyInput(input);  
 });
 
 const entries = document.querySelector('#carta-texto');
